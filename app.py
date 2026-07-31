020edbbe2eb0eb6f0b009bfc481aa0082d366264
Pranav-M-123 <pranavmaturi@gmail.com>
2026-07-31 15:28:51 -0400

    Integrated SQLite database for permanant storage

--- app.py
+++ app.py
@@ -1,41 +1,47 @@
 from flask import Flask, render_template, request
 from textblob import TextBlob
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///microblog.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(500), nullable=False)
+    mood = db.Column(db.String(50), nullable=False)
+    color = db.Column(db.String(20), nullable=False)
+
+with app.app_context():
+    db.create_all()
+
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    post_data = None
-    
-    # If the user clicked the "Publish Post" button:
     if request.method == 'POST':
-        # Grab the text they typed into the box
         text = request.form.get('content')
         
-        # AI Magic: Analyze the sentiment
         analysis = TextBlob(text)
         polarity = analysis.sentiment.polarity
         
-        # Assign a mood and a background color based on the AI's score
         if polarity > 0.1:
             mood = "Positive 😊"
-            color = "#d4edda" # Light green
+            color = "#d4edda"
         elif polarity < -0.1:
             mood = "Negative 😔"
-            color = "#f8d7da" # Light red
+            color = "#f8d7da"
         else:
             mood = "Neutral 😐"
-            color = "#e2e3e5" # Light grey
+            color = "#e2e3e5"
             
-        # Bundle it all up to send to the HTML page
-        post_data = {
-            "text": text,
-            "mood": mood,
-            "color": color
-        }
+        new_post = Post(text=text, mood=mood, color=color)
+        db.session.add(new_post)
+        db.session.commit()
         
-    # Send the bundled data to our index.html template
-    return render_template('index.html', post_data=post_data)
+    all_posts = Post.query.order_by(Post.id.desc()).all()
+    
+    return render_template('index.html', posts=all_posts)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    app.run(debug=True)
